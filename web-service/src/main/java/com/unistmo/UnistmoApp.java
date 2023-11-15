70b91b3951e38174a83cc95b37477a6c42373296
JoseDev <jotacastellanosguz@gmail.com>
2023-11-15 17:27:28 -0600

    Uploading

---
+++ web-service/src/main/java/com/unistmo/UnistmoApp.java
@@ -0,0 +1,11 @@
+package com.unistmo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnistmoApp {
+    public static void main(String args[]){
+        SpringApplication.run(UnistmoApp.class, args);
+    }
+}
