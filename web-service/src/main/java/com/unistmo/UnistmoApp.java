53cebe35403b1b70de89b32d5372e5de82ee11b7
JoseDev <jotacastellanosguz@gmail.com>
2023-11-16 10:39:24 -0600

    Se ajustan config para deploy en wildfly

--- web-service/src/main/java/com/unistmo/UnistmoApp.java
+++ web-service/src/main/java/com/unistmo/UnistmoApp.java
@@ -2,10 +2,17 @@ package com.unistmo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class UnistmoApp {
+public class UnistmoApp extends SpringBootServletInitializer {
     public static void main(String args[]){
         SpringApplication.run(UnistmoApp.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(UnistmoApp.class);
     }
 }
