64f6ff75b1665789f85a292acbe5d92c0b0c1297
José Castellanos <70968629+JoseCaste@users.noreply.github.com>
2023-11-17 12:13:48 -0600

    Update data.sql

--- web-service/src/main/resources/data.sql
+++ web-service/src/main/resources/data.sql
@@ -1,3 +1,9 @@
 insert into employees (id_employee, lastname, creation_date) values(100, 'José', now());
 insert into employees (id_employee, lastname, creation_date) values(101, 'José Castellanos', now());
 insert into employees (id_employee, lastname, creation_date) values(102, 'José Guzmán', now());
+insert into employees (id_employee, lastname, creation_date) values(103, 'José', now());
+insert into employees (id_employee, lastname, creation_date) values(104, 'José Castellanos', now());
+insert into employees (id_employee, lastname, creation_date) values(105, 'José Guzmán', now());
+insert into employees (id_employee, lastname, creation_date) values(106, 'José', now());
+insert into employees (id_employee, lastname, creation_date) values(107, 'José Castellanos', now());
+insert into employees (id_employee, lastname, creation_date) values(108, 'José Guzmán', now());
